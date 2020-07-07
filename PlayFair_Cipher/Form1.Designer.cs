96b98fa0ca8ccbae940ff5e7cf6086ec7e5c649a
aakyin <aakyin@student.cscc.edu>
2020-07-06 21:43:04 -0400

    first commit

---
+++ PlayFair_Cipher/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace PlayFair_Cipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSecretWord = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cipher \"Secret\" Word: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Input Text:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 419);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Output Text:";
+            // 
+            // txtSecretWord
+            // 
+            this.txtSecretWord.Location = new System.Drawing.Point(387, 62);
+            this.txtSecretWord.Multiline = true;
+            this.txtSecretWord.Name = "txtSecretWord";
+            this.txtSecretWord.Size = new System.Drawing.Size(376, 65);
+            this.txtSecretWord.TabIndex = 3;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(387, 192);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(764, 112);
+            this.txtInput.TabIndex = 4;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(396, 373);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(755, 111);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(61, 679);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(261, 81);
+            this.btnTranslate.TabIndex = 6;
+            this.btnTranslate.Text = "Translate Text";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(476, 679);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(241, 81);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(1317, 679);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(203, 81);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1621, 864);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtSecretWord);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Code Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSecretWord;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ PlayFair_Cipher/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayFair_Cipher
+{
+    //Alvin Akyin
+    //CSCI 1630
+    //03/26/2020
+    //The purpose of this project is to encrypt and decrypt text.
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int row = 5, col = 5;
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            char[,] table = new char[row, col];
+            string secretWord, input_Word, output_Word;
+            output_Word = "";
+            secretWord = txtSecretWord.Text;
+            table = populateTable(secretWord);
+            input_Word = txtInput.Text;
+            output_Word = txtOutput.Text;
+
+            if (input_Word != "" && output_Word == "")
+                txtOutput.Text = cipherText(table, input_Word);
+           }
+        
+            public string cipherText(char[,] myArray, string plaintext)
+            {
+                string cText = "";
+                int r = new int();
+                int c = new int();
+                for (int i = 0; i < plaintext.Length; i++)
+                {
+                    if (Char.IsLetter(plaintext[i]))
+                    {
+                        findLocation(myArray, Char.ToUpper(plaintext[i]), ref r, ref c);
+                        cText += myArray[c, r];
+                    }
+                    else
+                        cText += plaintext[i];
+                }
+
+                return cText;
+            }
+            public void findLocation(char[,] table, char ch, ref int r, ref int c)
+            {
+                if (ch == 'J')
+                    ch = 'I';
+
+                for (int i = 0; i < row; i++)
+                    for (int j = 0; j < col; j++)
+                    {
+                        if (table[i, j] == ch)
+                        {
+                            r = i;
+                            c = j;
+                            break;
+            }
+            }
+            }
+        public char[,] populateTable(string word)
+        {
+            char[,] table = new char[row, col];
+            string alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+            string wordWithoutDubplicate;
+            int i, j, k, l, m;
+            i = j = k = l = m = 0;
+            wordWithoutDubplicate = RemoveDuplicateChars(word);
+
+            wordWithoutDubplicate = wordWithoutDubplicate.ToUpper();
+
+            for (i = 0; i < wordWithoutDubplicate.Length; i++)
+            {
+                char c = wordWithoutDubplicate[i];
+                if (c == 'J')
+                    c = 'I';
+                if (k == 5)
+                {
+                    k = 0;
+                    j += 1;
+                }
+                table[j, k] = c;
+                k += 1;
+            }
+            l = k;
+
+            for (m = 0; m < alphabet.Length; m++)
+            {
+                if (l == 5)
+                {
+                    l = 0;
+                    j = j + 1;
+                }
+
+                if (findCharInTable(table, alphabet[m]) == false)
+                {
+                    table[j, l] = alphabet[m];
+                    l += 1;
+                }
+
+            }
+            return table;
+        }
+
+        public Boolean findCharInTable(Char[,] myArray, char c)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (myArray[i, j] == c)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+            txtOutput.Text = "";
+            txtSecretWord.Text = "";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public string RemoveDuplicateChars(string s)
+        {
+            string newString = string.Empty;
+            List<char> found = new List<char>();
+            foreach (char c in s)
+            {
+                if (found.Contains(c))
+                    continue;
+
+                newString += c.ToString();
+                found.Add(c);
+            }
+            return newString;
+        }
+    }
+    }
+
